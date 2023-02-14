dee4299e08ff33de392e55c9d2ece93ffb88f8d9
Joshua Donahue <joshua.a.donahue@gmail.com>
2023-02-14 16:02:16 -0500

    Add contact and about with app.get

--- server.js
+++ server.js
@@ -20,9 +20,20 @@ app.get("/",function(request, response){
 // This will now respond with hello when the request is made at "/". 
 // Now when going to "localhost:3000", the browser will make a request to get data back at "/" the home location. 
 // Then the callback function gets triggered and we send the browser the response "Hello" which shows on screen.
-app.get("/",function(request, response){ // Usually people will use "req", "res" for request and response here. req, res is best practice.
-    response.send("<h1>Hello World!</h1>"); // You could also send plain text "hello" instead of html, etc.
+app.get("/", function(req, res){ // Usually people will use "req", "res" for request and response here. req, res is best practice.
+    res.send("<h1>Hello World!</h1>"); // You could also send plain text "hello" instead of html, etc.
 });
+
+// This is the call back for someone trying to go to contact page. "http://localhost:3000/contact"
+app.get("/contact", function(req, res){
+    res.send("Contact me at: emailName@gmail.com");
+});
+
+// This is the call back for someone trying to go to contact page. "http://localhost:3000/about" Another route to a webpage.
+app.get("/about", function(req, res){
+    res.send("My name is Joshua Donahue and I'm a PROgrammer!");
+});
+    
 
 // This has the app listen to the port 3000. Can connect via cmd prompt with "node server.js" or whatever the js file is named.
 //app.listen(port); // If you connect via cmd line it will connect but not give feedback. Can ctrl + c to get back to normal cmd prompt
